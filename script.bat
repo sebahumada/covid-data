0ea0c410db685078986dccd55861f95517b140f8
Sebastian Ahumada <cbasmx@gmail.com>
2022-06-03 15:03:49 -0400

    `date +'m-d'`

--- script.bat
+++ script.bat
@@ -1,8 +1,6 @@
 @echo off
 C:\Proyectos\Covid\exec\DataCovid.exe
-current="`date +'%Y-%m-%d %H:%M:%S'`"
-msg="Updated: $current"
 git add .
-git commit -m "$msg"
+git commit -m "`date +'%Y-%m-%d'`"
 git push
 exit
